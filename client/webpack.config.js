a316bd4e0555a2ef55e23de67aa04b03dfafb376
K4ne <nick@getmighty.net>
2022-05-26 18:05:27 -0500

    Deleted assets folder and updates src-swjs, and webpack mostly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,22 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js'
       }),
+      new WebpackPwaManifest({
+        name: 'TODOs',
+        short_name: 'TODOs',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
